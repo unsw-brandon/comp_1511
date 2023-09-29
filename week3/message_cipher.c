e78262bbf02fd2b0aa492554a021baabc0a2db31
Brandon <brandonatipaishe1@gmail.com>
2023-09-30 01:56:09 +1000

    changes

--- week3/message_cipher.c
+++ week3/message_cipher.c
@@ -1,18 +1,161 @@
 #include <stdio.h>
 
-// This main function scans a message then enciphers/deciphers the message based
-// on user input
+void decipher(char letter1, char letter2, char letter3, char letter4) {
+    int number1, number2, number3, number4;
+    printf("Enter the numbers to decipher by: ");
+    scanf("%d %d %d %d", &number1, &number2, &number3, &number4);
+
+    // Decryption for letter1
+    if (letter1 >= 'A' && letter1 <= 'Z') {
+        int result1 = ((letter1 - 'A' - number1) % 26 + 'A');
+        if (result1 < 'A') {
+            result1 += 26;
+        }
+
+        printf("%c", result1);
+    } else if (letter1 >= 'a' && letter1 <= 'z') {
+        int result1 = ((letter1 - 'a' - number1) % 26 + 'a');
+        if (result1 < 'a') {
+            result1 += 26;
+        }
+
+        printf("%c", result1);
+    }
+
+    // Decryption for letter2
+    if (letter2 >= 'A' && letter2 <= 'Z') {
+        int result2 = ((letter2 - 'A' - number2) % 26 + 'A');
+        if (result2 < 'A') {
+            result2 += 26;
+        }
+        printf("%c", result2);
+    } else if (letter2 >= 'a' && letter2 <= 'z') {
+        int result2 = ((letter2 - 'a' - number2) % 26 + 'a');
+        if (result2 < 'a') {
+            result2 += 26;
+        }
+        printf("%c", result2);
+    }
+
+    // Decryption for letter3
+    if (letter3 >= 'A' && letter3 <= 'Z') {
+        int result3 = ((letter3 - 'A' - number3) % 26 + 'A');
+        if (result3 < 'A') {
+            result3 += 26;
+        }
+        printf("%c", result3);
+    } else if (letter3 >= 'a' && letter3 <= 'z') {
+        int result3 = ((letter3 - 'a' - number3) % 26 + 'a');
+        if (result3 < 'a') {
+            result3 += 26;
+        }
+        printf("%c", result3);
+    }
+
+    // Decryption for letter4
+    if (letter4 >= 'A' && letter4 <= 'Z') {
+        int result4 = ((letter4 - 'A' - number4) % 26 + 'A');
+        if (result4 < 'A') {
+            result4 += 26;
+        }
+        printf("%c", result4);
+    } else if (letter4 >= 'a' && letter4 <= 'z') {
+        int result4 = ((letter4 - 'a' - number4) % 26 + 'a');
+        if (result4 < 'a') {
+            result4 += 26;
+        }
+        printf("%c", result4);
+    }
+
+    printf("\n");
+}
+
+void encipher(char letter1, char letter2, char letter3, char letter4) {
+
+    int number1, number2, number3, number4;
+    printf("Enter the numbers to encipher by: ");
+    scanf("%d %d %d %d", &number1, &number2, &number3, &number4);
+
+    // Encryption for letter1
+    if (letter1 >= 'A' && letter1 <= 'Z') {
+        int result1 = ((letter1 - 'A' + number1) % 26 + 'A');
+        if (result1 < 'A') {
+            result1 += 26;
+        }
+
+        printf("%c", result1);
+    } else if (letter1 >= 'a' && letter1 <= 'z') {
+        int result1 = ((letter1 - 'a' + number1) % 26 + 'a');
+        if (result1 < 'a') {
+            result1 += 26;
+        }
+
+        printf("%c", result1);
+    }
+
+    // Encryption for letter2
+    if (letter2 >= 'A' && letter2 <= 'Z') {
+        int result2 = ((letter2 - 'A' + number2) % 26 + 'A');
+        if (result2 < 'A') {
+            result2 += 26;
+        }
+        printf("%c", result2);
+    } else if (letter2 >= 'a' && letter2 <= 'z') {
+        int result2 = ((letter2 - 'a' + number2) % 26 + 'a');
+        if (result2 < 'a') {
+            result2 += 26;
+        }
+        printf("%c", result2);
+    }
+
+    // Encryption for letter3
+    if (letter3 >= 'A' && letter3 <= 'Z') {
+        int result3 = ((letter3 - 'A' + number3) % 26 + 'A');
+        if (result3 < 'A') {
+            result3 += 26;
+        }
+        printf("%c", result3);
+    } else if (letter3 >= 'a' && letter3 <= 'z') {
+        int result3 = ((letter3 - 'a' + number3) % 26 + 'a');
+        if (result3 < 'a') {
+            result3 += 26;
+        }
+        printf("%c", result3);
+    }
+
+    // Encryption for letter4
+    if (letter4 >= 'A' && letter4 <= 'Z') {
+        int result4 = ((letter4 - 'A' + number4) % 26 + 'A');
+        if (result4 < 'A') {
+            result4 += 26;
+        }
+        printf("%c", result4);
+    } else if (letter4 >= 'a' && letter4 <= 'z') {
+        int result4 = ((letter4 - 'a' + number4) % 26 + 'a');
+        if (result4 < 'a') {
+            result4 += 26;
+        }
+        printf("%c", result4);
+    }
+
+    printf("\n");
+}
+
 int main(void) {
-    // Have a think about how we might implement this with a `struct message`
-    // once we've covered structs in the Thursday lecture!
-    char letter1;
-    char letter2;
-    char letter3;
-    char letter4;
-   
+    char letter1, letter2, letter3, letter4;
     printf("Message: ");
-    scanf(
-        "%c %c %c %c",
-        &letter1, &letter2, &letter3, &letter4
-    );
+    scanf("%c %c %c %c", &letter1, &letter2, &letter3, &letter4);
+
+    char choice;
+    printf("Would you like to encipher or decipher this message (e/d)? ");
+    scanf(" %c", &choice);
+
+    printf("\n");
+    if (choice == 'e') {
+        encipher(letter1, letter2, letter3, letter4);
+    } else if (choice == 'd') {
+        decipher(letter1, letter2, letter3, letter4);
+    }
+
+    return 0;
 }
--- week3/message_encipher.c
+++ week3/message_encipher.c
@@ -3,148 +3,75 @@
 int main() {
     char letter1, letter2, letter3, letter4;
     printf("Message: ");
-    scanf("%c%c%c%c", &letter1, &letter2, &letter3, &letter4);
+    scanf("%c %c %c %c", &letter1, &letter2, &letter3, &letter4);
 
     int number1, number2, number3, number4;
     printf("Enter the numbers to encipher by: ");
     scanf("%d %d %d %d", &number1, &number2, &number3, &number4);
 
-    int result1 = letter1 + number1;
-    int result2 = letter2 + number2;
-    int result3 = letter3 + number3;
-    int result4 = letter4 + number4;
-
-    // First Char
+    // Encryption for letter1
     if (letter1 >= 'A' && letter1 <= 'Z') {
-        if (result1 > 'Z') {
-            int value1 = result1 - 'Z' - 1;
-            result1 -= value1;
-
-            while (value1 > 25) {
-                value1 = result1 - 'Z' - 1;
-                result1 = result1 - value1;
-            }
-
-            result1 = 'A' + value1 + 1;
-
-        } else if (result1 < 'A') {
+        int result1 = ((letter1 - 'A' + number1) % 26 + 'A');
+        if (result1 < 'A') {
+            result1 += 26;
         }
+        
+        printf("%c", result1);
     } else if (letter1 >= 'a' && letter1 <= 'z') {
-
-        if (result1 > 25) {
-            int value1 = result1 - 'z' - 1;
-            result1 -= value1;
-
-            while (value1 > 'z') {
-                value1 = result1 - 'z' - 1;
-                result1 = result1 - value1;
-            }
-
-            result1 = 'a' + value1 + 1;
-
-        } else if (result1 < 'a') {
-            
-
-            
+        int result1 = ((letter1 - 'a' + number1) % 26 + 'a');
+        if (result1 < 'a') {
+            result1 += 26;
         }
+        
+        printf("%c", result1);
     }
 
-    // Second Char
+    // Encryption for letter2
     if (letter2 >= 'A' && letter2 <= 'Z') {
-        if (result2 > 'Z') {
-            int value2 = result2 - 'Z' - 1;
-            result2 -= value2;
-
-            while (value2 > 'Z') {
-                value2 = result2 - 'Z' - 1;
-                result2 = result2 - value2;
-            }
-
-            result2 = 'A' + value2 + 1;
-
-        } else if (result2 < 'A') {
+        int result2 = ((letter2 - 'A' + number2) % 26 + 'A');
+        if (result2 < 'A') {
+            result2 += 26;
         }
+        printf("%c", result2);
     } else if (letter2 >= 'a' && letter2 <= 'z') {
-
-        if (result2 > 'z') {
-            int value2 = result2 - 'z' - 1;
-            result2 -= value2;
-
-            while (value2 > 'z') {
-                value2 = result2 - 'z' - 1;
-                result2 = result2 - value2;
-            }
-
-            result2 = 'a' + value2 + 1;
-
-        } else if (result2 < 'a') {
+        int result2 = ((letter2 - 'a' + number2) % 26 + 'a');
+        if (result2 < 'a') {
+            result2 += 26;
         }
+        printf("%c", result2);
     }
 
-    // Third Char
+    // Encryption for letter3
     if (letter3 >= 'A' && letter3 <= 'Z') {
-        if (result3 > 'Z') {
-            int value3 = result3 - 'Z' - 1;
-            result3 -= value3;
-
-            while (value3 > 'Z') {
-                value3 = result3 - 'Z' - 1;
-                result3 = result3 - value3;
-            }
-
-            result3 = 'A' + value3 + 1;
-
-        } else if (result3 < 'A') {
+        int result3 = ((letter3 - 'A' + number3) % 26 + 'A');
+        if (result3 < 'A') {
+            result3 += 26;
         }
+        printf("%c", result3);
     } else if (letter3 >= 'a' && letter3 <= 'z') {
-        if (result3 > 'z') {
-            int value3 = result3 - 'z' - 1;
-            result3 -= value3;
-
-            while (value3 > 'z') {
-                value3 = result3 - 'z' - 1;
-                result3 = result3 - value3;
-            }
-
-            result3 = 'a' + value3 + 1;
-
-        } else if (result3 < 'a') {
+        int result3 = ((letter3 - 'a' + number3) % 26 + 'a');
+        if (result3 < 'a') {
+            result3 += 26;
         }
+        printf("%c", result3);
     }
 
-    // Fourth Char
+    // Encryption for letter4
     if (letter4 >= 'A' && letter4 <= 'Z') {
-        if (result4 > 'Z') {
-            int value4 = result4 - 'Z' - 1;
-            result4 -= value4;
-
-            while (value4 > 'Z') {
-                value4 = result4 - 'Z' - 1;
-                result4 = result4 - value4;
-            }
-
-            result4 = 'A' + value4 + 1;
-
-        } else if (result4 < 'A') {
+        int result4 = ((letter4 - 'A' + number4) % 26 + 'A');
+        if (result4 < 'A') {
+            result4 += 26;
         }
+        printf("%c", result4);
     } else if (letter4 >= 'a' && letter4 <= 'z') {
-
-        if (result4 > 'z') {
-            int value4 = result4 - 'z' - 1;
-            result4 -= value4;
-
-            while (value4 > 'z') {
-                value4 = result4 - 'z' - 1;
-                result4 = result4 - value4;
-            }
-
-            result4 = 'a' + value4 + 1;
-
-        } else if (result4 < 'a') {
+        int result4 = ((letter4 - 'a' + number4) % 26 + 'a');
+        if (result4 < 'a') {
+            result4 += 26;
         }
+        printf("%c", result4);
     }
 
-    printf("%c%c%c%c\n", result1, result2, result3, result4);
+    printf("\n");
 
     return 0;
 }
